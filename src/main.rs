2b069a46e3f3f7571ab139ad61b609b9fa7d4c2a
Josh Burns <joshyburnss@gmail.com>
2024-05-15 22:13:53 +1000

    swap actix out for actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::collections::HashMap;
+use std::sync::Mutex;
+use actix_web::{web};
 
 #[derive(Parser, Debug)]
 #[command(author = "Josh Burns", version = "0.0.0", about = "Mini key-value store", long_about = None)]
@@ -6,14 +9,22 @@ struct Args {
     #[arg(long, default_value_t = String::from("0.0.0.0"))]
     host: String,
 
-    #[arg(long, default_value_t = 8899)]
+    #[arg(long, short, default_value_t = 8899)]
     port: u16,
 }
 
-#[actix::main]
+struct AppState {
+    store: Mutex<HashMap<String, String>>
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    println!("{:#?}", args);
+    let state = web::Data::new(AppState {
+	store: Mutex::new(HashMap::new())
+    });
+
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     Ok(())
 }
