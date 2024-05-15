bf7e4388bc5c77833474bda72c9b9896527eafef
Josh Burns <joshyburnss@gmail.com>
2024-05-15 22:07:01 +1000

    setup base CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author = "Josh Burns", version = "0.0.0", about = "Mini key-value store", long_about = None)]
+struct Args {
+    #[arg(long, default_value_t = String::from("0.0.0.0"))]
+    host: String,
+
+    #[arg(long, default_value_t = 8899)]
+    port: u16,
+}
+
+#[actix::main]
+async fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    println!("{:#?}", args);
+
+    Ok(())
 }
