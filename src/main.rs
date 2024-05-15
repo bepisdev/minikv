59bddc91f213eaba632cbf5b687bb175161b514a
Josh Burns <joshyburnss@gmail.com>
2024-05-15 23:46:28 +1000

    use web path instead of request URI for key id

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::collections::HashMap;
 use std::sync::Mutex;
-use actix_web::{web, App, get, post, delete, HttpServer, HttpRequest, HttpResponse};
+use actix_web::{web, App, get, post, delete, HttpServer, HttpResponse};
 use actix_web::http::header::ContentType;
 use log::info;
 
@@ -15,69 +15,74 @@ struct Args {
     port: u16,
 }
 
+#[derive(Debug)]
 struct AppState {
     store: Mutex<HashMap<String, String>>
 }
 
 #[get("/{key:.*}")]
-async fn get(request: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
-    let key: String = request.uri().to_string();
+async fn get(path: web::Path<String>, data: web::Data<AppState>) -> HttpResponse {
+    let key: String = path.into_inner();
+
+    info!("Fetching {key}");
 
     match data.store.lock().unwrap().get(&key) {
 	Some(value) => HttpResponse::Ok()
-	    .content_type(ContentType::plaintext())
-	    .body(value.to_string()),
+			.content_type(ContentType::plaintext())
+			.body(value.to_string()),
 	_ => HttpResponse::NotFound()
-	    .content_type(ContentType::plaintext())
-	    .body("key not found"),
+		.content_type(ContentType::plaintext())
+		.body("key not found\n")
     }
+
 }
 
 #[post("/{key:.*}")]
-async fn set(request: HttpRequest, post: web::Bytes, data: web::Data<AppState>) -> HttpResponse {
-    let key: String = request.uri().to_string();
+async fn set(path: web::Path<String>, post: web::Bytes, data: web::Data<AppState>) -> HttpResponse {
+    let key: String = path.into_inner();
     let value: String = String::from_utf8(post.to_vec()).unwrap();
+
+    info!("Setting {key} as {value}");
 
     match data.store.lock().unwrap().insert(key, value) {
 	None => HttpResponse::Created()
 	    .content_type(ContentType::plaintext())
-	    .body("new value inserted"),
+	    .body("new value inserted\n"),
 	Some(_) => HttpResponse::Accepted()
 	    .content_type(ContentType::plaintext())
-	    .body("value updated"),
+	    .body("value updated\n"),
     }
 }
 
 #[delete("/{key:.*}")]
-async fn del(request: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
-    let key: String = request.uri().to_string();
+async fn del(path: web::Path<String>, data: web::Data<AppState>) -> HttpResponse {
+    let key: String = path.into_inner();
 
     match data.store.lock().unwrap().remove(&key) {
 	Some(_) => HttpResponse::Ok()
 	    .content_type(ContentType::plaintext())
-	    .body("key and value removed"),
+	    .body("key and value removed\n"),
 	_ => HttpResponse::NotFound()
 	    .content_type(ContentType::plaintext())
-	    .body("key not found"),
+	    .body("key not found\n"),
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    let state = web::Data::new(AppState {
-	store: Mutex::new(HashMap::new())
-    });
-
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    HttpServer::new(move || {
+    let _ = HttpServer::new(move || {
+	let state = web::Data::new(AppState {
+	    store: Mutex::new(HashMap::new())
+	});
+
         App::new()
-            .app_data(state.clone())
+            .app_data(state)
             .service(get)
             .service(set)
             .service(del)
-            .wrap(info!("%a %{User-Agent}i"))
     })
     .bind((args.host, args.port))?
     .run()
