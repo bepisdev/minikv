b5c7b7052469211e89496d9d6f9a2995f0086c24
Josh Burns <joshyburnss@gmail.com>
2024-05-18 18:12:26 +1000

    use environment variables exclusively to set username and password, emit warning if using defaults

--- README.md
+++ README.md
@@ -24,13 +24,11 @@ MiniKV is configurable via CLI flags and environment variables.
 
 - `-p` or `--port` - Defines TCP/IP port to bind too (Default is `8899`).
 - `--host` - Defines interface IP address to bind too (Default is `0.0.0.0` - leave this alone unless you know what you're doing).
-- `--username` - Defines the *HTTP Basic Auth* username to use for authentication.
-- `--password` - Defines the *HTTP Basic Auth* password to use for authentication.
 
 #### Environment Variables
 
-- `MINIKV_USERNAME` - Same as `--username`
-- `MINIKV_PASSWORD` - Same as `--password`
+- `MINIKV_USERNAME` - Defines the *HTTP Basic Auth* username to use for authentication.
+- `MINIKV_PASSWORD` - Defines the *HTTP Basic Auth* password to use for authentication.
 
 ## Installation
 
--- src/middlewares.rs
+++ src/middlewares.rs
@@ -1,13 +1,24 @@
 use actix_web::{dev::ServiceRequest, error::ErrorUnauthorized, Error as ActixError};
 use actix_web_httpauth::extractors::basic::BasicAuth;
+use log::warn;
+use std::env;
 
 pub async fn auth_middleware(
     req: ServiceRequest,
     creds: BasicAuth,
 ) -> Result<ServiceRequest, (ActixError, ServiceRequest)> {
-    if creds.user_id() == "admin" && creds.password() == Some("admin") {
+
+    // Fetch the valid credentials from environment variables
+    let valid_user_id = env::var("MINIKV_USERNAME").unwrap_or_else(|_| "admin".to_string());
+    let valid_password = env::var("MINIKV_PASSWORD").unwrap_or_else(|_| "admin".to_string());
+
+    if creds.user_id() == valid_user_id && creds.password() == Some(valid_password.as_str()) {
+	if creds.user_id() == "admin" && creds.password() == Some("admin") {
+	    warn!("Using default credentials is not secure. This is fine for development and testing but when using in production environments consider using a different username and password. This can be changed with environment variables MINIKV_USERNAME and MINIKV_PASSWORD.");
+	}
         Ok(req)
     } else {
+	warn!("Unauthorized access attempt with user ID: {}", creds.user_id());
         Err((ErrorUnauthorized("Invalid Credentials"), req))
     }
 }
